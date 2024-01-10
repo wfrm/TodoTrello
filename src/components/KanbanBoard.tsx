3b1fea9def1827fc202e9f4cab26ae37b8882748
wfrm <willmar.romero@gmail.com>
2024-01-10 12:15:12 -0500

    poner conteneddores para la funcion de drag and drop

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,21 +1,44 @@
 import PlusIcon from "../icons/PlusIcon"
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Column, Id } from "../types";
 import ColumnContainer from "./ColumnContainer";
+import {
+    DndContext,
+    DragEndEvent,
+    DragOverEvent,
+    DragOverlay,
+    DragStartEvent,
+    PointerSensor,
+    useSensor,
+    useSensors,
+  } from "@dnd-kit/core";
+
+  import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+
 function KanbanBoard() {
     const [columns, setColumns] = useState<Column[]>([]);//(defaultCols);
     console.log(columns);
+    const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px] justify-center">
+        <DndContext>
         <div className="m-auto flex gap-4">
-            <div className="flex gap-4">{columns.map(col =>(
-            <ColumnContainer  key={col.id} column={col} deleteColumn={deleteColumn}/>
-            ))}
+
+            <div className="flex gap-4">
+                <SortableContext children={undefined} items={columnsId}>
+                {columns.map(col =>(
+                    <ColumnContainer  key={col.id} column={col} deleteColumn={deleteColumn}/>
+                ))}
+                </SortableContext>
+
+
             </div>
                 <button onClick={()=>{createNewColumn();}} className=" h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg border-2   border-columnBackgroundColor bg-mainBackgroundColor p4 ring-rose-500 hover:ring-2 flex gap-2 " > <PlusIcon/>Add column
                 
                 </button>
         </div>
+        </DndContext>
+
 
     </div>
   )

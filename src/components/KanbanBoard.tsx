60d91ff1da2d270f859a7f9a640b33190131c7c7
wfrm <willmar.romero@gmail.com>
2024-01-10 11:07:25 -0500

    css para el contenedor de las columnas

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -7,7 +7,7 @@ function KanbanBoard() {
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px] justify-center">
         <div className="m-auto ">
-            <div>{columns.map(col =><div>{col.title}</div>)}</div>
+            <div className="flex gap-2">{columns.map(col =><div>{col.title}</div>)}</div>
                 <button onClick={()=>{createNewColumn();}} className=" h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg border-2   border-columnBackgroundColor bg-mainBackgroundColor p4 ring-rose-500 hover:ring-2 flex gap-2 " > <PlusIcon/>Add column
                 
                 </button>
